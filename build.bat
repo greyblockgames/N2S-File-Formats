72edab8297b91b5dc6e140a360857bc68e2d3013
Lukas Olson <lukasdragon@live.ca>
2020-06-28 14:30:45 -0700

    Updated schema for new tilemap implementation

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-flatc -n SaveSchema.fbs --gen-onefile
-flatc -n helpers/DataTypes.fbs --gen-onefile
+flatc  -o build/standard/ -n src/SaveSchema.fbs --gen-onefile --gen-all
+flatc  -o build/objectAPI/ -n src/SaveSchema.fbs --gen-object-api --gen-onefile --gen-all
 @pause
